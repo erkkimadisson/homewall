cf6c0946eb12a9cfa20c245258ed1fe8c0c70371
Erkki Madisson <admin@erkkidev.fi>
2024-10-28 09:10:45 +0200

    split the head of the html to its own file

---
+++ essentials.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/98a9f8b1f8.js" crossorigin="anonymous"></script>
+    <script src="./public/js/nav.js" defer></script>
+
+    <link rel="stylesheet" href="./public/css/main.css">
+    <title><?= $title ?></title>
+</head>
